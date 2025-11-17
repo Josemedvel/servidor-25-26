b1363147c596e76df9c0f018283dd783c3da6a97
Josemedvel <josemedvel@gmail.com>
2025-11-17 18:08:08 +0100

    repaso 14 noviembre

--- ut7_8/repaso/repaso.js
+++ ut7_8/repaso/repaso.js
@@ -1,13 +1,15 @@
+"use strict"; // Activa el modo estricto
+
 import os from "os"
-console.log(os.platform())
+//console.log(os.platform())
 
 let num = 3
 const numero = 1
 const apellidos = 'medina'
-console.log(numero + apellidos)
+//console.log(numero + apellidos)
 let array = new Array(1,2,3)
 let array_2 = [1,2,3]
-console.log(array_2)
+//console.log(array_2)
 
 /*for(let i = 0; i < array_2.length; i++){
     console.log(array_2[i])
@@ -15,15 +17,32 @@ console.log(array_2)
 function cada_numero(num){
     console.log(num)
 }
-array.forEach(num => {
+/*array.forEach(num => {
     console.log(num)
     console.log(-num)
 })
-array_2.forEach(cada_numero)
+*/
+//array_2.forEach(cada_numero)
 
 
 function saludo(){
     console.log("hola")
 }
-setInterval(saludo, 1000)
-set
+//setInterval(saludo, 1000)
+
+function cuentaAtras(segundos, callback){
+    let iteraciones = 0;
+    function iterar(){
+        if(iteraciones != segundos){
+            console.log(segundos-iteraciones,"...")
+            iteraciones++;
+            setTimeout(iterar, 1000)
+        }else{
+            callback()
+        }
+    }
+    iterar()
+}
+
+cuentaAtras(3, saludo)
+
